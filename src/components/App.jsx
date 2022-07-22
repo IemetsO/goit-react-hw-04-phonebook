b2ca198b55144589e166de6a13ee5c224cf81aab
IemetsO <oksana.v.iemets@gmail.com>
2022-07-22 15:45:24 +0300

    Update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,8 +6,8 @@ import { nanoid } from 'nanoid';
 import { useState, useEffect} from "react"
 
 const useLocalStorage = (key,defaultValue) => {
-  const [state, setState] = useState(() => {
-    return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue})
+  const [state, setState] = useState(() => 
+    JSON.parse(window.localStorage.getItem(key)) ?? defaultValue)
 
     useEffect(()=>{
       window.localStorage.setItem(key, JSON.stringify(state))
